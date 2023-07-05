6a62c7a4ab0a6f3570cc0c84070d374485f1b4fb
Yubin-23 <137861704+Yubin-23@users.noreply.github.com>
2023-07-05 21:45:52 +0900

    Update StudentView.java

--- src/com/iu/study4/s2/StudentView.java
+++ src/com/iu/study4/s2/StudentView.java
@@ -1,5 +1,9 @@
 package com.iu.study4.s2;
 //학생 출력
 public class StudentView {
-
+	public void print(Student[] sa) {
+		for(int i=0; i<sa.length; i++) {
+			System.out.println("이름: "+sa[i].name+" 번호: "+sa[i].num+" 총점: "+sa[i].total+" 평균: "+sa[i].avg);
+		}
+	}
 }
