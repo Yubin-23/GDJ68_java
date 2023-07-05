5cd3e22130b3fa6b0af33731c70be6f0dc6dc0a9
Yubin-23 <137861704+Yubin-23@users.noreply.github.com>
2023-07-05 21:44:56 +0900

    Update StudentControllor.java

--- src/com/iu/study4/s2/StudentControllor.java
+++ src/com/iu/study4/s2/StudentControllor.java
@@ -11,7 +11,28 @@ public class StudentControllor {
 		//3.프로그램 종료
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("1.학생정보 생성 2.학생정보 출력 3.프로그램 종료");
-		int select = sc.nextInt();
+		Student[] sa = new Student[0];
+		
+		while(true) {
+			System.out.println("1.학생정보 생성 2.학생정보 출력 3.프로그램 종료");
+			int select = sc.nextInt();
+			
+			if(select==1) {
+				StudentService ss = new StudentService();
+				sa = ss.makeStudents();
+				
+			}else if(select==2) {
+				if(sa.length!=0) {
+					StudentView sv = new StudentView();
+					sv.print(sa);
+				}else {
+					System.out.println("학생정보가 없습니다");
+					break;
+				}
+			}else {
+				break;
+			}
+		}
+		System.out.println("프로그램 종료");
 	}
 }
